661c78d3d196c35698a534fd6d916769f9edb700
Aboubakary833 <aboubakarycisse472@gmail.com>
2026-01-19 18:24:32 +0000

    fix: migrations syntax issue

--- migrations/00002_create_posts_table.sql
+++ migrations/00002_create_posts_table.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS posts (
-    id              BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),,
+    id              BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
     user_id         BINARY(16) NOT NULL,
     title           VARCHAR(150) NOT NULL,
     slug            VARCHAR(200) NOT NULL UNIQUE,
